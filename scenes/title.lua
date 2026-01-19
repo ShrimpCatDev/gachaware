34232500abf3bbc2df053af4537b5adb7abb7371
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-01-19 15:33:30 -0600

    add ball adder function

--- scenes/title.lua
+++ scenes/title.lua
@@ -1,11 +1,22 @@
 local title={}
 
-function title:enter()
-
+function title:newBall(x,y)
+    table.insert(self.balls,{})
+    self.balls[#self.balls].body=lp.newBody(self.world,x,y,"dynamic")
+    self.balls[#self.balls].shape=lp.newCircleShape(15)
+    self.balls[#self.balls].fixture=lp.newFixture(self.balls[#self.balls].body,self.balls[#self.balls].shape,1)
 end
 
-function title:update()
+function title:enter()
+    self.world=lp.newWorld(0,9.81*64,true)
+    self.ball={
+        balls={},
+        img=lg.newImage("assets/title/ball-1.png")
+    }
+end
 
+function title:update(dt)
+    self.world:update(dt)
 end
 
 function title:draw()
