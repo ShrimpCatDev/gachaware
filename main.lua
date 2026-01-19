36d81968ef312f5fa8723d3dbc4bd549e867967a
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-01-19 15:24:21 -0600

    add palette and title state

--- main.lua
+++ main.lua
@@ -1,9 +1,19 @@
 require("init")
 
 function love.load()
+    gs.registerEvents()
     font=lg.newFont("assets/font/monogram-extended.ttf",16)
     lg.setFont(font)
     shove.createLayer("game")
+
+    pal=require("lib/pal")
+    pal:new("waft",li.newImageData("assets/palette/waft.png"))
+    pal:load("waft")
+
+    state={
+        title=require "scenes/title"
+    }
+    gs.switch(state.title)
 end
 
 function love.update(dt)
@@ -11,6 +21,5 @@ function love.update(dt)
 end 
 
 function love.draw()
-    beginDraw()
-    endDraw()
+
 end
---
+++ scenes/title.lua
@@ -0,0 +1,17 @@
+local title={}
+
+function title:enter()
+
+end
+
+function title:update()
+
+end
+
+function title:draw()
+    beginDraw()
+        lg.clear(pal:color(6))
+    endDraw()
+end
+
+return title
