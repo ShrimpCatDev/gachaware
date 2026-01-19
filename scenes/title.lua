961e0b3b6d24d200694106930d569f28ab6d7184
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-01-19 16:34:52 -0600

    trippy uwu

--- scenes/title.lua
+++ scenes/title.lua
@@ -12,8 +12,18 @@ end
 
 function title:draw()
     beginDraw()
-        lg.clear(pal:color(6))
+        lg.clear(pal:color(0))
         lg.setColor(1,1,1,1)
+
+        local t=love.timer.getTime()
+        local sc=10
+        
+        for x=-10,20 do
+            for y=-10,20 do
+                lg.setColor(pal:color((x+y-8)%16))
+                lg.circle("fill",x*sc+math.cos(t+(x+y)*0.2)*20,y*sc+math.sin(t+(x+y)*0.2)*20,10+math.sin(t+(x+y)*0.8)*5)
+            end
+        end
     endDraw()
 end
 
