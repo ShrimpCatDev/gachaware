bdcfab098aba7819ef90785c2c1b39fb0bd01f9f
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-02-09 18:12:58 -0600

    did this for no reason lol

--- data/machines.lua
+++ data/machines.lua
@@ -1,4 +1,4 @@
 return {
     {id="tanuki",name="Tanuki Land",img=assets.image.gachaMachineBase},
-    {id="tanuki",name="Florp Yes",img=assets.image.cursedGacha}
+    --{id="tanuki",name="Florp Yes",img=assets.image.cursedGacha}
 }
