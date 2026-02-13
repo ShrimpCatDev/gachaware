f3e57eab3181278aee223900695799dc588fa1a7
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-02-13 11:10:56 -0600

    add menu options

--- pause.lua
+++ pause.lua
@@ -1,21 +1,28 @@
 local pause={}
 
-function pause:init(type,data)
-    self.open=false
-    self.selection=1
-    self.items={}
-
+function pause:loadMenu(type,data)
+    self.menuItems={}
+    table.insert(self.menuItems,{text="Resume",func=function(selection)
+        self.open=false
+        self.gy=self.dy
+        sfx(assets.sfx.menuClose)
+    end})
+    table.insert(self.menuItems,{text="Options",func=function(selection)
+        self.items=self.menuOptions
+        self.selection=1
+        sfx(assets.sfx.click)
+    end})
     if type=="menu" then
-        table.insert(self.items,{text="Back to title",func=function(selection)
+        table.insert(self.menuItems,{text="Back to title",func=function(selection)
             sfx(assets.sfx.menuClose)
             data.prog=6
-            timer.tween(1,data,{prog=-4},"out-cubic",function()
-                local g=data.machineMenu.data[selection].id
+            timer.tween(1,data,{prog=-4},"out-cubic",function(selection)
+                --local g=data.machineMenu.data[selection].id
                 gs.switch(state.title)
             end)
         end})
     elseif type=="game" then
-        table.insert(self.items,{text="Back to menu",func=function(selection)
+        table.insert(self.menuItems,{text="Back to menu",func=function(selection)
             sfx(assets.sfx.menuClose)
             data.tScale=0
             timer.tween(0.5,data,{tScale=20},"in-linear",function() 
@@ -23,11 +30,40 @@ function pause:init(type,data)
             end)
         end})
     end
-    table.insert(self.items,{text="Resume",func=function(selection)
-        self.open=false
-        self.gy=self.dy
-        sfx(assets.sfx.menuClose)
+end
+
+function pause:loadOptions(type,data)
+    self.menuOptions={}
+    table.insert(self.menuOptions,{text="Volume: 10",func=function(selection)
+        options.volume=options.volume+0.1
+        if options.volume>1 then options.volume=0 end
+        sfx(assets.sfx.click)
     end})
+    table.insert(self.menuOptions,{text="Music: 10",func=function(selection)
+        options.musicVolume=options.musicVolume+0.1
+        if options.musicVolume>1 then options.musicVolume=0 end
+        sfx(assets.sfx.click)
+    end})
+    table.insert(self.menuOptions,{text="SFX: 10",func=function(selection)
+        options.sfxVolume=options.sfxVolume+0.1
+        if options.sfxVolume>1 then options.sfxVolume=0 end
+        sfx(assets.sfx.click)
+    end})
+    table.insert(self.menuOptions,{text="Back",func=function(selection)
+        sfx(assets.sfx.menuClose)
+        self.items=self.menuItems
+        self.selection=1
+    end})
+end
+
+function pause:init(type,data)
+    self.open=false
+    self.selection=1
+    self.items={}
+
+    self:loadMenu(type,data)
+    self:loadOptions(type,data)
+    self.items=self.menuItems
 
     self.dx=0
     self.dy=-conf.gH
@@ -39,10 +75,15 @@ function pause:init(type,data)
     self.x=self.gx
     self.y=self.gy
     self.w=64
-    self.h=#self.items*(fontDlg:getHeight()+1)+16
+    self.gh=#self.items*(fontDlg:getHeight()+1)+16
+    self.h=self.gh
 end
 
 function pause:update(dt)
+    self.gh=#self.items*(fontDlg:getHeight()+1)+16
+    self.menuOptions[1].text="Volume: "..options.volume*10
+    self.menuOptions[2].text="Music: "..options.musicVolume*10
+    self.menuOptions[3].text="SFX: "..options.sfxVolume*10
     if self.open then
         if input:pressed("exit") or input:pressed("b") then
             self.open=false
@@ -82,6 +123,7 @@ function pause:update(dt)
     end
     self.x=lerpDt(self.x,self.gx,0.005,dt)
     self.y=lerpDt(self.y,self.gy,0.005,dt)
+    self.h=lerpDt(self.h,self.gh,0.000005,dt)
 end
 
 function pause:draw()
@@ -99,7 +141,8 @@ function pause:draw()
 
         for k,v in ipairs(self.items) do
             if k==self.selection then
-                lg.print("-"..v.text,conf.gW/2-(fontDlg:getWidth(v.text)/2),k*fontDlg:getHeight()+conf.gH/2-(self.h/2)+6)
+                local t="-"..v.text
+                lg.print(t,conf.gW/2-(fontDlg:getWidth(t)/2),k*fontDlg:getHeight()+conf.gH/2-(self.h/2)+6)
             else
                 lg.print(v.text,conf.gW/2-(fontDlg:getWidth(v.text)/2),k*fontDlg:getHeight()+conf.gH/2-(self.h/2)+6)
             end
