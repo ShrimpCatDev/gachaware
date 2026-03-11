990e47bb6931770888ac44769f27188afe08ca79
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-03-11 09:19:40 -0500

    testies

--- data/infoDialog.lua
+++ data/infoDialog.lua
@@ -6,12 +6,12 @@ return{
         "A tanuki, a bowl of ramen... and OOH a shiny blue tanuki! ^.^",
         "I make customers play this one first because its a good introdution to GachaWare."
     },
-    --[[{
+    {
         "Oh gosh...",
         "What even is this",
         "I dont even remember putting this one here.",
         "OH I KNOW WHY ITS HERE",
         "The developer must have needed it to test something!",
         "I... think..."
-    }]]
+    }
 }
