caf14d203602356abcae1c40833978e1759f966a
ShrimpCatDev <shrimpcatboi223@outlook.com>
2026-03-14 14:44:07 -0500

    add build stuff

---
+++ build.lua
@@ -0,0 +1,9 @@
+return {
+      name = 'GachaWare',
+      config = 'nonsteam',
+      version = '0.9',
+      love = '11.5',
+      icon = 'assets/icon.png',
+      platforms = {'windows','linux','macos'},
+      ignore = {".git"}
+}
